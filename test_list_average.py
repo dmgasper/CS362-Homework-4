58b326c7d21fd2b35f3cfa856160954d1949ab47
David Gasper <gasperd@oregonstate.edu>
2021-02-07 19:27:07 -0800

    Reorder tests

--- test_list_average.py
+++ test_list_average.py
@@ -3,13 +3,13 @@ import list_average
 
 class TestListAverage(unittest.TestCase):
 
-	def test_empty_list(self):
-		result = list_average.list_average({})
-		self.assertEqual(result, None)
-		
 	def test_filled_list(self):
 		result = list_average.list_average({0, 2, 7})
 		self.assertEqual(result, 3)
+
+	def test_empty_list(self):
+		result = list_average.list_average({})
+		self.assertEqual(result, None)
 		
 	def test_non_number(self):
 		result = list_average.list_average({0, 2, "hello"})
